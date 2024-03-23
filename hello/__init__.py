1ea24fb13dca9d596b1956223acc4af24f796725
kykysuperfly23 <89097049+kykysuperfly23@users.noreply.github.com>
2024-03-23 18:59:32 +0800

    Update __init__.py

--- hello/__init__.py
+++ hello/__init__.py
@@ -5,3 +5,16 @@ from flask_login import LoginManager
 
 db = SQLAlchmey()
 DB_NAME = "database.db"
+
+def create_app():
+  app = Flask(__name__)
+  app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+  app.config['SQALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+  db.init_app(app)
+
+  from .views import views
+  from .auth import auth
+
+  app.register_blueprint(views, url_prefix='/')
+  app.register_blueprint(auth, url_prefix='/')
+
