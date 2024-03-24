d6f6a84dfa828a98e7aea492954e5ff13c97a224
Maryana Malyushytska <maryanamaly@gmail.com>
2024-03-24 11:45:54 +0800

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-#importing website package...
-
-from website import create_app
-
-app = create_app()
-
-#only runs when the file is ran directly, not just imported.
-if __name__ == 'main':
-  app.debug(debug = True) #any changes made will rerun the server
